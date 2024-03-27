dc965e9d3f6c73d331b2a8de27cfad3d6c300641
W.Bischoff <w.bischoff@geze.com>
2024-03-27 18:02:55 +0100

    SemAnt added while test

---
+++ src/test/resources/while.cpp
@@ -0,0 +1,39 @@
+int funcInt() {
+    return 3;
+}
+
+char funcChar() {
+    return 'c';
+}
+
+int main()
+{
+    // ok
+    while (true) {
+    //while (false) {
+    //while (!true) {
+    //while (!false) {
+    //while (1) {
+    //while (funcInt()) {
+    //while (1 > 0) {
+    //while ('c' > 'c') {
+    //while (1 > funcInt()) {
+    //while ('c' > funcChar()) {
+    //while (funcInt() == funcInt()) {
+    //while (funcInt() != funcInt()) {
+
+    // error
+    //while ('c') {
+    //while (funcChar()) {
+    //while (1 > 'c') {
+    //while ('c' > 1) {
+    //while (1 > 1) {
+    //while (1 > funcChar()) {
+    //while (1 < funcChar()) {
+    //while ('c' > funcInt()) {
+    //while ('c' < funcInt()) {
+    //while (funcInt() == funcChar()) {
+    //while (funcInt() != funcChar()) {
+        funcChar();
+    }
+}
